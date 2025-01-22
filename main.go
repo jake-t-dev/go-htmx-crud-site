f7d3e5ae9be0a077b440b75d82003f8c4e2e7738
jake-t-dev <jake.turner@dailypay.com>
2025-01-22 10:11:05 +0000

    connect db and create templates

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
+var db *sql.DB
+
+func initDB() {
+	var err error
+	db, err = sql.Open("mysql", "root:root@(127.0.0.1:3333)/testdb")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
+
+	initDB()
+	defer db.Close()
 
 	router := mux.NewRouter()
 
@@ -18,5 +38,11 @@ func main() {
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the home page!")
+	var version string
+
+	if err := db.QueryRow("SELECT VERSION()").Scan(&version); err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write([]byte("db version: " + version))
 }
