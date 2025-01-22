012631495413c223421aa535b100e72547b80bd5
jake-t-dev <jake.turner@dailypay.com>
2025-01-22 11:22:41 +0000

    initial commit

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -12,6 +13,12 @@ import (
 
 var db *sql.DB
 var tmpl *template.Template
+
+type Task struct {
+	Id   int
+	Task string
+	Done bool
+}
 
 func init() {
 	var err error
@@ -45,6 +52,8 @@ func main() {
 
 	router.HandleFunc("/", HomeHandler)
 
+	router.HandleFunc("/tasks", fetchTasks).Methods("GET")
+
 	http.ListenAndServe(":3000", router)
 
 }
@@ -55,4 +64,44 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, "Error executing teamplate: "+err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func fetchTasks(w http.ResponseWriter, r *http.Request) {
+	todos, _ := getTasks(db)
+	fmt.Println(todos)
+	tmpl.ExecuteTemplate(w, "todoList", todos)
+}
+
+func getTasks(dbPointer *sql.DB) ([]Task, error) {
+	query := "SELECT id, task, done FROM tasks"
+
+	rows, err := dbPointer.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var tasks []Task
+
+	for rows.Next() {
+		var todo Task
+
+		rowErr := rows.Scan(&todo.Id, &todo.Task, &todo.Done)
+
+		if rowErr != nil {
+			return nil, rowErr
+		}
+
+		tasks = append(tasks, todo)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	fmt.Println(tasks)
+
+	return tasks, nil
 }
